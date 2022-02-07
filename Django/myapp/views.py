fd2cbc19df9869842c64f7d0092dee2bfa08a87b
shinjangwoon <wkddns1225@naver.com>
2022-02-07 19:12:50 +0900

    title, body, submit 추가

--- Django/myapp/views.py
+++ Django/myapp/views.py
@@ -1,15 +1,54 @@
 from django.shortcuts import render, HttpResponse
 import random
-# Create your views here.
+topics = [
+    {'id': 1, 'title': 'routing', 'body': 'Routing is ...'},
+    {'id': 2, 'title': 'view', 'body': 'View is ...'},
+    {'id': 3, 'title': 'model', 'body': 'Model is ...'},
+]
+
+
+def HTMLTemplate(articleTag):
+    global topics
+    ol = ''
+    for topic in topics:
+        ol += f'<li><a href="/read/{topic["id"]}">{topic["title"]}</a></li>'
+
+    return f'''
+    <html>
+    <body>
+        <h1><a href="/">Django</a></h1>
+        <ul>
+            {ol}
+        </ul>
+        {articleTag}
+    </body>
+    </html>
+    '''
 
 
 def index(request):
-    return HttpResponse('<h1>Random</h1>'+str(random.random()))
-
-
-def create(request):
-    return HttpResponse('Create')
+    article = ''' 
+    <h2>Welcome</h2>
+    Hello, Django
+    '''
+    return HttpResponse(HTMLTemplate(article))
 
 
 def read(request, id):
-    return HttpResponse('Read!'+id)
+    global topics
+    article = ''
+    for topic in topics:
+        if topic['id'] == int(id):
+            article = f'<h2>{topic["title"]}</h2>{topic["body"]}'
+    return HttpResponse(HTMLTemplate(article))
+
+
+def create(request):
+    article = '''
+        <form action="/create/">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p><textarea name="body" placeholder="body"></textarea></p>
+            <p><input type="submit"></p>
+        </form>
+    '''
+    return HttpResponse(HTMLTemplate(article))
