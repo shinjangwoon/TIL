fdfa498dae1ab919de0e575dfde14ca13aed9ae6
shinjangwoon <wkddns1225@naver.com>
2022-02-07 17:53:46 +0900

    Django 복습

---
+++ Django/myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+import random
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1>Random</h1>'+str(random.random()))
+
+
+def create(request):
+    return HttpResponse('Create')
+
+
+def read(request, id):
+    return HttpResponse('Read!'+id)
